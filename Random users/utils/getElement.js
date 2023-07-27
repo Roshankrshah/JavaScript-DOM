91e482e46046ec0813e1c37262bc028dc98d793c
Roshan <shahroshan0720@gmail.com>
2023-07-27 12:24:15 +0530

    getelement function

---
+++ Random users/utils/getElement.js
@@ -0,0 +1,8 @@
+const getElement = () => {
+    const element = document.querySelector(selection);
+    if (element)
+        return element;
+    throw new Error('no element selected');
+}
+
+export default getElement;
