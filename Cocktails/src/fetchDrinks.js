fef5937a9c7fdf17d197be4b7128feea91cd88ed
Roshan <shahroshan0720@gmail.com>
2023-07-29 15:09:49 +0530

    fetch data

---
+++ Cocktails/src/fetchDrinks.js
@@ -0,0 +1,12 @@
+const fetchDrinks = async (url)=>{
+    showloading();
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }catch(error){
+        console.log(error);
+    }
+};
+
+export default fetchDrinks;
