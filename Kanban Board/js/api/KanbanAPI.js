11d5b1f04dded924937e28a921f8ebe9acb543a9
Roshan <shahroshan0720@gmail.com>
2023-10-18 18:29:22 +0530

    delete data function

--- Kanban Board/js/api/KanbanAPI.js
+++ Kanban Board/js/api/KanbanAPI.js
@@ -58,6 +58,19 @@ export default class KanbanAPI{
         }
         save(data);
     }
+
+    static deleteItem(itemId){
+        const data = read();
+
+        for(const column of data){
+            const item = column.items.find(item => item.id == itemId);
+            
+            if(item){
+                column.items.splice(column.items.indexOf(item),1);
+            }
+        }
+        save(data);
+    }
 }
 
 function read(){
