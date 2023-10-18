bbd569e1165123fb61e80fe328188a06920b6410
Roshan <shahroshan0720@gmail.com>
2023-10-18 18:25:41 +0530

    insert data

--- Kanban Board/js/api/KanbanAPI.js
+++ Kanban Board/js/api/KanbanAPI.js
@@ -8,6 +8,56 @@ export default class KanbanAPI{
 
         return column.items;
     }
+
+    static insertItem(columnId, content){
+        const data = read();
+        const column = data.find(column => column.id == columnId);
+        const item = {
+            id: Math.floor(Math.random()*1000),
+            content
+        };
+
+        if(!column){
+            throw new Error('Column does not exist.');
+        }
+
+        column.items.push(item);
+        save(data);
+
+        return item;
+    }
+
+    static updateItem(itemId, newProps){
+        const data = read();
+        const [item, currentColumn] = (()=>{
+            for (const column of data){
+                const item = column.items.find(item => item.id == itemId);
+
+                if(item){
+                    return [item, column];
+                }
+            }
+        })();
+        
+        if(!item){
+            throw new Error('Item not found');
+        }
+
+        item.content = newProps.content === undefined ? item.content : newProps.content;
+
+        if(newProps.columnId !== undefined && newProps.position !== undefined){
+            const targetColumn = data.find(column => column.id == newProps.columnId);
+
+            if(!targetColumn){
+                throw new Error('Target column not found');
+            }
+
+            currentColumn.items.splice(currentColumn.items.indexOf(item),1);
+
+            targetColumn.items.splice(newProps.position,0,item);
+        }
+        save(data);
+    }
 }
 
 function read(){
--- Kanban Board/js/main.js
+++ Kanban Board/js/main.js
@@ -1,3 +1,1 @@
 import KanbanAPI from "./api/KanbanAPI.js";
-
-console.log(KanbanAPI.getItems(1));
