ebb63f448b6320c29613b4bc1eb9df38ef709c45
Roshan <shahroshan0720@gmail.com>
2023-10-28 20:35:33 +0530

    error handle

--- Pdf Viewer/js/main.js
+++ Pdf Viewer/js/main.js
@@ -2,7 +2,7 @@ const url = '../docs/pdf.pdf';
 
 let pdfDoc = null, pageNum = 1, pageIsRendering = false, pageNumIsPending = null;
 
-const scale = 1.5, canvas = document.querySelector('#pdf-render'),
+const scale = 1.2, canvas = document.querySelector('#pdf-render'),
     ctx = canvas.getContext('2d');
 
 const renderPage = num =>{
@@ -61,6 +61,14 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
     document.querySelector('#page-count').textContent = pdfDoc.numPages;
 
     renderPage(pageNum);
+})
+.catch(err=>{
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div,canvas);
+
+    document.querySelector('.top-bar').style.display = 'none';
 });
 
 document.querySelector('#prev-page').addEventListener('click',showPrevPage);
